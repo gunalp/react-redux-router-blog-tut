907c76f063a2f548c3348f2860c7511ce3fd60ea
alp_uysal <uysalp@gmail.com>
2018-06-10 01:06:41 +0300

    mm

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux';
+import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
+  posts:PostsReducer
+
+
+
+  /* dummy reducer
   state: (state = {}) => state
+  */
 });
 
 export default rootReducer;
